9481f93ae9d237e6af3da600f2de9e3bc0fe5903
Kevin Babcock and Malgorzata Haniszewska <kevin@>
2017-10-31 15:38:32 -0700

    add forms for player q uit quit exit stop q

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+function Player (name) {
+  this.name = name;
+  this.totals = [];
+}
+
+Player.prototype.addScore = function(currentTotal) {
+  var playerScore = 0;
+  this.totals.forEach(function(total) {
+    playerScore += total;
+  })
+  return playerScore;
+}
+
+$(document).ready(function() {
+
+  var currentTotal = 0;
+  var player1;
+  var player2;
+
+  $("#playerNames").submit(function(event) {
+    event.preventDefault();
+
+    namePlayer1 = $("#player1").val();
+    namePlayer2 = $("#player2").val();
+
+    var player1 = new Player(namePlayer1);
+    var player2 = new Player(namePlayer2);
+    //console.log(player1);
+    //console.log(player2);
+    $("#playerNames").hide();
+    $(".scores").show();
+    $(".play").show();
+  })
+
+  $("#roll").click(function(event) {
+    var number = Math.ceil(Math.random() * 6);
+    $("#rollResult").text(number);
+    if (number === 1) {
+      currentTotal = 0;
+      $("#roll").hide();
+    } else {
+      currentTotal += number;
+    }
+  })
+
+  $("#hold").click(function(event) {
+    player1.totals.push(currentTotal);
+    $("#player1-score").text(player1.addScore(currentTotal));
+  })
+
+
+
+})
